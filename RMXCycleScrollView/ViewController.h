f249d4cd9c5a41764ca33eacab3f8e4de32c3d7e
xiaojian <zhiling1991@gmail.com>
2018-04-10 14:21:28 +0800

    Initial Commit

---
+++ RMXCycleScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RMXCycleScrollView
+//
+//  Created by Shawnge on 2018/4/10.
+//  Copyright © 2018年 Shawnge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RMXCycleScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RMXCycleScrollView
+//
+//  Created by Shawnge on 2018/4/10.
+//  Copyright © 2018年 Shawnge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
